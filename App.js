5981c4425ecc7c06c40a90aa5bcb5c0709c023fe
RandilT <randiltharusha72@gmail.com>
2024-02-03 13:07:20 +0530

    add new fonts

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
+import { useCallback } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Make your changes on App.js</Text>
+      <Text style={styles.text}>Make your changesas sdsds sdson App.js</Text>
       <StatusBar style="auto" />
     </View>
   );
